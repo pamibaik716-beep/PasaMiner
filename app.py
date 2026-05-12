4ba26a6f8669acaeee2f9190a07d9f5f7a702200
pamibaik716-beep <pamibaik716@gmail.com>
2026-05-12 09:39:16 +0800

    app.py

--- app.py
+++ app.py
@@ -877,8 +877,9 @@ def admin_logout():
 # RUN
 # =========================================
 
-app.run(
-    host="0.0.0.0",
-    port=5000,
-    debug=True
-)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
